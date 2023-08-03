4925bdfcde6fdec6a1482614a04e1ee2ab8a870e
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-08-03 19:52:24 +0000

    Bump version: 0.2.1 → 0.2.2

--- mediamanager/settings/app_settings.py
+++ mediamanager/settings/app_settings.py
@@ -21,7 +21,7 @@ class AppSecrets(BaseSettings):
 
 class AppSettings(BaseSettings):
     app_title = "MediaManager"
-    app_version = "0.2.1"
+    app_version = "0.2.2"
 
     db_file: str = "/data/media_manager.db"
     default_user_email: str = "changeme@email.com"
