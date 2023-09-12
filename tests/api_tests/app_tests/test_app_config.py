ca509063996df81f095ddbb24e6e3a7137f844c2
Michael Genson <71845777+michael-genson@users.noreply.github.com>
2023-09-12 21:22:56 +0000

    added test

--- tests/api_tests/app_tests/test_app_config.py
+++ tests/api_tests/app_tests/test_app_config.py
@@ -3,7 +3,7 @@ from fastapi.testclient import TestClient
 from mediamanager.models.app.app_config import AppConfig
 from mediamanager.routes import app_config
 from mediamanager.services.factory import ServiceFactory
-from tests.utils.generators import random_url
+from tests.utils.generators import random_int, random_string, random_url
 
 
 def test_get_app_config(api_client: TestClient, svcs: ServiceFactory, auth_headers: dict):
@@ -29,3 +29,36 @@ def test_update_app_config(api_client: TestClient, auth_headers: dict):
     r.raise_for_status()
     fetched_config = AppConfig.parse_obj(r.json())
     assert fetched_config == updated_config
+
+
+def test_update_app_config_monitored_libraries(api_client: TestClient, auth_headers: dict):
+    monitored_library_ids = [random_string() for _ in range(random_int(3, 5))]
+
+    r = api_client.get(app_config.router.url_path_for("get_app_config"), headers=auth_headers)
+    r.raise_for_status()
+    original_config = AppConfig.parse_obj(r.json())
+
+    # set monitored library ids
+    updated_config = AppConfig(**original_config.dict())
+    updated_config.monitored_library_ids = monitored_library_ids
+    r = api_client.put(
+        app_config.router.url_path_for("update_app_config"), json=updated_config.dict(), headers=auth_headers
+    )
+    r.raise_for_status()
+
+    r = api_client.get(app_config.router.url_path_for("get_app_config"), headers=auth_headers)
+    r.raise_for_status()
+    fetched_config_data: dict = r.json()
+    assert fetched_config_data["monitoredLibraryIds"] == monitored_library_ids
+
+    # clear monitored library ids
+    updated_config.monitored_library_ids = []
+    r = api_client.put(
+        app_config.router.url_path_for("update_app_config"), json=updated_config.dict(), headers=auth_headers
+    )
+    r.raise_for_status()
+
+    r = api_client.get(app_config.router.url_path_for("get_app_config"), headers=auth_headers)
+    r.raise_for_status()
+    fetched_config_data = r.json()
+    assert fetched_config_data.get("monitoredLibraryIds") is None
